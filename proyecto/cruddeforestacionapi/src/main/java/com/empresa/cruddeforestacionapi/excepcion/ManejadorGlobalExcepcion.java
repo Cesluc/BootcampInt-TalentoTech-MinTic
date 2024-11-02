bccaa88326ea7996ce231c6b208c8060989efc50
Cesluc <clucumi.94@gmail.com>
2024-11-02 15:55:44 -0500

    Manejo de excepciones creados

--- proyecto/cruddeforestacionapi/src/main/java/com/empresa/cruddeforestacionapi/excepcion/ManejadorGlobalExcepcion.java
+++ proyecto/cruddeforestacionapi/src/main/java/com/empresa/cruddeforestacionapi/excepcion/ManejadorGlobalExcepcion.java
@@ -1,5 +1,21 @@
 package com.empresa.cruddeforestacionapi.excepcion;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class ManejadorGlobalExcepcion {
     
+    // Se define la clase que va a manejar ese tipo de excepciones
+    @ExceptionHandler(RecursoNoEncontradoExcepcion.class)
+    public ResponseEntity<?> manejadorRecursoNoEncontrado(RecursoNoEncontradoExcepcion recursoNoEncontradoExcepcion) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(recursoNoEncontradoExcepcion.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> manejadorExcepcionGlobal(Exception exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error interno del servidor");
+    }
 }
--- proyecto/cruddeforestacionapi/src/main/java/com/empresa/cruddeforestacionapi/excepcion/RecursoNoEncontradoExcepcion.java
+++ proyecto/cruddeforestacionapi/src/main/java/com/empresa/cruddeforestacionapi/excepcion/RecursoNoEncontradoExcepcion.java
@@ -1,5 +1,10 @@
 package com.empresa.cruddeforestacionapi.excepcion;
 
-public class RecursoNoEncontradoExcepcion {
+public class RecursoNoEncontradoExcepcion extends RuntimeException {
+
+    // private static final long serialVersionID = 1L;
     
+    public RecursoNoEncontradoExcepcion(String mensaje){
+        super(mensaje);
+    }
 }
