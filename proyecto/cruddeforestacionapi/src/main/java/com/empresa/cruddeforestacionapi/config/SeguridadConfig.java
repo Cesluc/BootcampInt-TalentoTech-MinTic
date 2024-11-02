4c152aacae8e86d1c0c1633d1534ca97bd8f60ce
Cesluc <clucumi.94@gmail.com>
2024-11-02 14:13:28 -0500

    Configurado el modulo de Seguridad

--- proyecto/cruddeforestacionapi/src/main/java/com/empresa/cruddeforestacionapi/config/SeguridadConfig.java
+++ proyecto/cruddeforestacionapi/src/main/java/com/empresa/cruddeforestacionapi/config/SeguridadConfig.java
@@ -1,5 +1,52 @@
 package com.empresa.cruddeforestacionapi.config;
 
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity // activamos para configurarla
 public class SeguridadConfig {
     
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        
+        httpSecurity
+            .cors(cors -> cors.configurationSource(corsConfigurationSource())) //Trabajaremos con los cors de manera personalizada
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(authorize -> authorize
+                                    // .requestMatchers("/api/**").authenticated()
+                                    .anyRequest().permitAll())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        //JWT
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    private CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        //Ips Permitidos para configurar nuestra api
+        //Setteamos atributos del objeto
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type")); //Es convertido en JSON todo lo recibido.
+        configuration.setAllowCredentials(true); //Para temas de cookies y tokens
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
 }
