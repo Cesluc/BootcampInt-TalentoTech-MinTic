a4a3cec5c31b66c6ad2921d26d1a9733fc449042
Cesluc <clucumi.94@gmail.com>
2024-11-02 17:37:33 -0500

    Creacion de retos

---
+++ Retos/RetoI2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+/*
+* Recomendaciones Generales:
+*
+*    -> El método run() funcionará como nuestro método principal
+*    -> No declarar objetos de tipo Scanner, utilizar el método read() para solicitar datos al usuario.
+*    -> Si requiere utilizar varias clases, estas NO deben ser tipo public.
+*/
+class RetoI2 {
+
+    /**
+     * Este debe ser el único objeto de tipo Scanner en el código
+     */
+    private final Scanner scanner = new Scanner(System.in);
+
+    /**
+     * Este método es usado para solicitar datos al usuario
+     * 
+     * @return Lectura de la siguiente linea del teclado
+     */
+    public String read() {
+        return this.scanner.nextLine();
+    }
+
+    /**
+     * método principal
+     */
+    public void run() {
+        /*
+         * solución propuesta
+         */
+    }
+
+    public static void main(String[] args) {
+        RetoI2 retoI2 = new RetoI2();
+        retoI2.run();
+    }
+}
---
+++ Retos/RetoP1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class RetoP1 {
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    public String read() {
+        return this.scanner.nextLine();
+    }
+
+    public void run() {
+        /*
+         * solución propuesta
+         */
+    }
+
+    public static void main(String[] args) {
+        RetoP1 retoP1 = new RetoP1();
+        retoP1.run();
+    }
+    
+}
