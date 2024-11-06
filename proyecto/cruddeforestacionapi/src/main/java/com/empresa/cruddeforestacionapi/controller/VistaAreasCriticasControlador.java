a2f81c6cb4fe5e0eee30f9c4f0718bff97a40f23
Cesluc <clucumi.94@gmail.com>
2024-11-05 21:20:42 -0500

    Creacion de Vista Areas Criticas

--- proyecto/cruddeforestacionapi/src/main/java/com/empresa/cruddeforestacionapi/controller/VistaAreasCriticasControlador.java
+++ proyecto/cruddeforestacionapi/src/main/java/com/empresa/cruddeforestacionapi/controller/VistaAreasCriticasControlador.java
@@ -1,5 +1,23 @@
 package com.empresa.cruddeforestacionapi.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.empresa.cruddeforestacionapi.dto.VistaAreaCriticaDTO;
+import com.empresa.cruddeforestacionapi.service.VistasAreasCriticasServicio;
+
+@RestController
+@RequestMapping("/api/vista-areas-criticas")
 public class VistaAreasCriticasControlador {
-    
+    @Autowired
+    private VistasAreasCriticasServicio vistasAreasCriticasServicio;
+
+    @GetMapping
+    public List<VistaAreaCriticaDTO> obtenerVistaAreasCriticas() {
+        return vistasAreasCriticasServicio.obtenerVistaAreasCriticas();
+    }
 }
--- proyecto/cruddeforestacionapi/src/main/java/com/empresa/cruddeforestacionapi/util/DatabaseInitializer.java
+++ proyecto/cruddeforestacionapi/src/main/java/com/empresa/cruddeforestacionapi/util/DatabaseInitializer.java
@@ -1,5 +1,33 @@
 package com.empresa.cruddeforestacionapi.util;
 
-public class DatabaseInitializer {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import jakarta.persistence.EntityManager;
+
+@Component
+public class DatabaseInitializer implements CommandLineRunner {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Transactional // Transacciones
+    @Override
+    public void run(String... args) throws Exception {
+
+        entityManager.createNativeQuery(
+            "CREATE OR REPLACE VIEW vista_areas_criticas_con_evaluaciones AS " +
+            "SELECT ac.id, ac.nombre, ac.descripcion, COUNT(ef.id) AS numero_evaluaciones " +
+            "FROM areas_criticas ac " +
+            "LEFT JOIN evaluaciones_ferroviarias ef " +
+            "ON ac.id = ef.area_critica_id " + 
+            "GROUP BY ac.id, ac.nombre, ac.descripcion " +
+            "HAVING numero_evaluaciones > 0"
+        ).executeUpdate();
+
+    }
     
+
 }
